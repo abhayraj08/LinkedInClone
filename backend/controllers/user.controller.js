5223bdb6782ac8788d38336139c2910185d48d9b
abhayraj08 <mathematics7878@gmail.com>
2026-06-24 14:52:24 +0530

    add login route & testing

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,13 +2,14 @@ import User from "../models/user.model.js"
 import Profile from "../models/profile.model.js"
 
 import bcrypt from "bcrypt";
+import crypto from "crypto";
 
 
 export const register = async (req, res) => {
     try {
         const { name, email, password, username } = req.body;
 
-        if (name || email || password || username) return res.status(400).json({ message: "Enter the required field.." });
+        if (!name || !email || !password || !username) return res.status(400).json({ message: "Enter the required field.." });
 
         const user = await User.findOne({
             email
@@ -17,7 +18,7 @@ export const register = async (req, res) => {
         if (user) return res.status(400).json({ message: "User already exists" });
 
 
-        const hasedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = new User({
             name,
             email,
@@ -34,6 +35,31 @@ export const register = async (req, res) => {
         res.json({ message: "User created successfully" });
 
     } catch (err) {
-        return res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: err.message });
+    }
+}
+
+export const login = async (req, res) => {
+
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) return res.status(400).json({ message: "Enter the required field.." });
+
+        const user = await User.findOne({ email });
+
+        if (!user) return res.status(404).json({ message: "User does not exit!!" });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ message: "Invalid Credentials" });
+
+        const token = crypto.randomBytes(32).toString("hex");
+
+        await User.findByIdAndUpdate(user._id, token);
+
+        return res.json({ token });
+
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
     }
 }
